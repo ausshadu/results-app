5b936655701d5311f2898bf08be3c2b9f1aaf17b
Shadab Mehdi <ausshadu@gmail.com>
2025-10-28 18:49:58 +0530

    Finally made some meaningful change

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-  distDir: "dist",
+  output: "export",
 };
 
 export default nextConfig;
